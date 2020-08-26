f1e8ffff609b10b116d60d3a8d53415badab45e4
DanArcherOnline <danarcheronline@gmail.com>
2020-08-26 21:22:36 +0900

    add comments to ticker

--- lib/domain/ticker.dart
+++ lib/domain/ticker.dart
@@ -2,7 +2,14 @@ class Ticker {
   Stream<int> tick({int ticks}) {
     return Stream.periodic(
       Duration(seconds: 1),
-      (x) => ticks - x - 1,
+
+      // The first call/tick passes [numOfEvents] back with the
+      // value of 0, rather than 1.
+      // Therefore, the [-1] offsets the final value back to what
+      // it should be
+      (numOfEvents) => ticks - numOfEvents - 1,
+      //[take()] sets a maximum of events. When the provided number
+      //of events are emitted, the stream will finish.
     ).take(ticks);
   }
 }
